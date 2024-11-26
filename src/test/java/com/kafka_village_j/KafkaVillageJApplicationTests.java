04ed111b10d03cb0c7309c377336cd912a45c36f
castelbaker <castellabakery@gmail.com>
2024-11-26 17:32:58 +0900

    concurrency test

--- src/test/java/com/kafka_village_j/KafkaVillageJApplicationTests.java
+++ src/test/java/com/kafka_village_j/KafkaVillageJApplicationTests.java
@@ -1,13 +1,95 @@
 package com.kafka_village_j;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kafka_village_j.wood.WoodService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class KafkaVillageJApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    WoodService woodService;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void insertList() throws JsonProcessingException {
+        for (int i = 0; i < 100; i++) {
+            String request = "{" +
+                    "\"uuid\":\"id" + i + "\"," +
+                    "\"name\":\"name" + i + "\"," +
+                    "\"age\":\"" + i + "\"" +
+                    "}";
+            woodService.create(new ObjectMapper().readTree(request));
+        }
+    }
+
+    @Test
+    void updateList() throws JsonProcessingException {
+        for (int i = 0; i < 100; i++) {
+            String request = "{" +
+                    "        \"filter\": {" +
+                    "            \"uuid\": \"id1\"" +
+                    "        }," +
+                    "        \"action\": {" +
+                    "            \"uuid\": \"id2\"," +
+                    "            \"name\": \"name2\"," +
+                    "            \"age\": \"32\"" +
+                    "        }" +
+                    "    }";
+            woodService.update(new ObjectMapper().readTree(request));
+        }
+    }
+
+    @Test
+    void incrementList() throws InterruptedException {
+        int threadCount = 10000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i=0;i<threadCount;i++) {
+            executorService.submit(() -> {
+                String request = "{" +
+                        "        \"filter\": {" +
+                        "            \"uuid\": \"id1\"" +
+                        "        }," +
+                        "        \"action\": {" +
+                        "            \"age\": -1" +
+                        "        }," +
+                        "        \"key\": \"$inc\"" +
+                        "    }";
+                try {
+                    woodService.update(new ObjectMapper().readTree(request));
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+    }
+
+    @Test
+    void deleteList() throws JsonProcessingException {
+        for (int i = 0; i < 100; i++) {
+            String request = "{" +
+                    "\"filter\": {" +
+                    "\"uuid\":\"id" + i + "\"" +
+                    "}" +
+                    "}";
+            woodService.delete(new ObjectMapper().readTree(request));
+        }
+    }
 
 }
